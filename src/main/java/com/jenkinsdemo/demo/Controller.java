58d05e72e07faf6f75ab5129e4ccb46468e630f9
MEHMET AKGÜNGÖR <99881339+Mehmett03@users.noreply.github.com>
2024-05-02 14:32:08 +0300

    uyawdsad

--- src/main/java/com/jenkinsdemo/demo/Controller.java
+++ src/main/java/com/jenkinsdemo/demo/Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping
     public String deneme2(){
-        return "feıouk";
+        return "feısadasdouasdasdk";
     }
 }
