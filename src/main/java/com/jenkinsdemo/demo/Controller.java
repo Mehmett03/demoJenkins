0c273eae75779109f51ee607c00493278ce005e1
MEHMET AKGÜNGÖR <99881339+Mehmett03@users.noreply.github.com>
2024-05-02 14:28:25 +0300

    uyawd

--- src/main/java/com/jenkinsdemo/demo/Controller.java
+++ src/main/java/com/jenkinsdemo/demo/Controller.java
@@ -1,4 +1,14 @@
 package com.jenkinsdemo.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+    @GetMapping
+    public String deneme2(){
+        return "feıouk";
+    }
 }
